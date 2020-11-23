08d2934075b39c7c27caea0e1865f957a2740cce
Eduardo <eduardo20041995@hotmail.com>
2020-11-23 19:24:58 -0400

    Adiciona Scheduler Dev Express

--- src/Faina.Principal.pas
+++ src/Faina.Principal.pas
@@ -15,14 +15,138 @@ uses
   Vcl.Dialogs,
   Vcl.ExtCtrls,
   Vcl.ComCtrls,
-  Vcl.Buttons;
+  Vcl.Buttons,
+  Vcl.Menus,
+  Data.DB,
+  System.ImageList,
+  Vcl.ImgList,
+  System.Actions,
+  Vcl.ActnList,
+
+  cxGraphics,
+  cxControls,
+  cxLookAndFeels,
+  cxLookAndFeelPainters,
+  cxStyles,
+  cxEdit,
+  cxScheduler,
+  cxSchedulerStorage,
+  cxSchedulerCustomControls,
+  cxSchedulerCustomResourceView,
+  cxSchedulerDayView,
+  cxSchedulerAgendaView,
+  cxSchedulerDateNavigator,
+  cxSchedulerHolidays,
+  cxSchedulerTimeGridView,
+  cxSchedulerUtils,
+  cxSchedulerWeekView,
+  cxSchedulerYearView,
+  cxSchedulerGanttView,
+  cxSchedulerRecurrence,
+  dxBarBuiltInMenu,
+  cxSchedulerTreeListBrowser,
+  cxSchedulerRibbonStyleEventEditor,
+  dxSkinsCore,
+  dxSkinsDefaultPainters,
+  dxRibbonSkins,
+  dxRibbonCustomizationForm,
+  cxImageList,
+  dxPrinting,
+  cxSchedulerActions,
+  dxActions,
+  dxBar,
+  cxClasses,
+  dxRibbon;
 
 type
   TPrincipal = class(TForm)
     pnlTop: TPanel;
     tvwMenu: TTreeView;
     SpeedButton1: TSpeedButton;
+    cxScheduler1: TcxScheduler;
+    dxRibbon1: TdxRibbon;
+    dxBarManager1: TdxBarManager;
+    ActionList1: TActionList;
+    cxImageList1: TcxImageList;
+    cxImageList2: TcxImageList;
+    dxSchedulerNewEvent: TdxSchedulerNewEvent;
+    dxRibbonTabHome: TdxRibbonTab;
+    dxBarEvent: TdxBar;
+    dxBarLargeButtonNewEvent: TdxBarLargeButton;
+    dxSchedulerNewRecurringEvent: TdxSchedulerNewRecurringEvent;
+    dxBarLargeButtonNewRecurringEvent: TdxBarLargeButton;
+    dxSchedulerGoBackward: TdxSchedulerGoBackward;
+    dxBarNavigation: TdxBar;
+    dxBarLargeButtonGoBackward: TdxBarLargeButton;
+    dxSchedulerGoForward: TdxSchedulerGoForward;
+    dxBarLargeButtonGoForward: TdxBarLargeButton;
+    dxSchedulerGoToToday: TdxSchedulerGoToToday;
+    dxBarLargeButtonGotoToday: TdxBarLargeButton;
+    dxSchedulerGoToDate: TdxSchedulerGoToDate;
+    dxBarLargeButtonGotoDate: TdxBarLargeButton;
+    dxSchedulerNextSevenDays: TdxSchedulerNextSevenDays;
+    dxBarLargeButtonNext7Days: TdxBarLargeButton;
+    dxSchedulerDayView: TdxSchedulerDayView;
+    dxBarArrange: TdxBar;
+    dxBarLargeButtonDay: TdxBarLargeButton;
+    dxSchedulerWorkWeekView: TdxSchedulerWorkWeekView;
+    dxBarLargeButtonWorkWeek: TdxBarLargeButton;
+    dxSchedulerWeekView: TdxSchedulerWeekView;
+    dxBarLargeButtonWeek: TdxBarLargeButton;
+    dxSchedulerMonthView: TdxSchedulerMonthView;
+    dxBarLargeButtonMonth: TdxBarLargeButton;
+    dxSchedulerTimeGridView: TdxSchedulerTimeGridView;
+    dxBarLargeButtonTimeline: TdxBarLargeButton;
+    dxSchedulerYearView: TdxSchedulerYearView;
+    dxBarLargeButtonYear: TdxBarLargeButton;
+    dxSchedulerGanttView: TdxSchedulerGanttView;
+    dxBarLargeButtonGanttView: TdxBarLargeButton;
+    dxSchedulerAgendaView: TdxSchedulerAgendaView;
+    dxBarLargeButtonAgenda: TdxBarLargeButton;
+    dxSchedulerGroupByNone: TdxSchedulerGroupByNone;
+    dxBarGroupBy: TdxBar;
+    dxBarLargeButtonGroupByNone: TdxBarLargeButton;
+    dxSchedulerGroupByDate: TdxSchedulerGroupByDate;
+    dxBarLargeButtonGroupByDate: TdxBarLargeButton;
+    dxSchedulerGroupByResource: TdxSchedulerGroupByResource;
+    dxBarLargeButtonGroupByResource: TdxBarLargeButton;
+    dxRibbonTabView: TdxRibbonTab;
+    dxBarTimeScale: TdxBar;
+    dxBarSubItem1: TdxBarSubItem;
+    dxSchedulerTimeScale60Minutes: TdxSchedulerTimeScale60Minutes;
+    dxBarLargeButton60Minutes: TdxBarLargeButton;
+    dxSchedulerTimeScale30Minutes: TdxSchedulerTimeScale30Minutes;
+    dxBarLargeButton30Minutes: TdxBarLargeButton;
+    dxSchedulerTimeScale15Minutes: TdxSchedulerTimeScale15Minutes;
+    dxBarLargeButton15Minutes: TdxBarLargeButton;
+    dxSchedulerTimeScale10Minutes: TdxSchedulerTimeScale10Minutes;
+    dxBarLargeButton10Minutes: TdxBarLargeButton;
+    dxSchedulerTimeScale6Minutes: TdxSchedulerTimeScale6Minutes;
+    dxBarLargeButton6Minutes: TdxBarLargeButton;
+    dxSchedulerTimeScale5Minutes: TdxSchedulerTimeScale5Minutes;
+    dxBarLargeButton5Minutes: TdxBarLargeButton;
+    dxSchedulerCompressWeekends: TdxSchedulerCompressWeekends;
+    dxBarLayout: TdxBar;
+    dxBarLargeButtonCompressWeekends: TdxBarLargeButton;
+    dxSchedulerWorkTimeOnly: TdxSchedulerWorkTimeOnly;
+    dxBarLargeButtonWorkingHours: TdxBarLargeButton;
+    dxSchedulerSnapEventsToTimeSlots: TdxSchedulerSnapEventsToTimeSlots;
+    dxBarLargeButtonSnapEventsToTimeSlots: TdxBarLargeButton;
+    dxSchedulerDateNavigator: TdxSchedulerDateNavigator;
+    dxBarLargeButtonDateNavigator: TdxBarLargeButton;
+    dxSchedulerResourcesLayoutEditor: TdxSchedulerResourcesLayoutEditor;
+    dxBarLargeButtonResourcesLayoutEditor: TdxBarLargeButton;
+    dxSchedulerShowPrintForm: TdxSchedulerShowPrintForm;
+    dxRibbonTabFile: TdxRibbonTab;
+    dxBarPrint: TdxBar;
+    dxBarLargeButtonPrint: TdxBarLargeButton;
+    dxSchedulerShowPrintPreviewForm: TdxSchedulerShowPrintPreviewForm;
+    dxBarLargeButtonPrintPreview: TdxBarLargeButton;
+    dxSchedulerShowPageSetupForm: TdxSchedulerShowPageSetupForm;
+    dxBarLargeButtonPageSetup: TdxBarLargeButton;
+    cxSchedulerStorage: TcxSchedulerStorage;
     procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
   public
@@ -35,13 +159,22 @@ var
 implementation
 
 uses
-  Faina.Login;
+  Faina.Login,
+  Faina.Dados;
 
 {$R *.dfm}
 
 procedure TPrincipal.FormShow(Sender: TObject);
 begin
   TLogin.New(Self);
+
+  if FileExists('Scheduler.data') then
+    cxSchedulerStorage.LoadFromFile('Scheduler.data');
+end;
+
+procedure TPrincipal.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  cxSchedulerStorage.SaveToFile('Scheduler.data');
 end;
 
 end.
